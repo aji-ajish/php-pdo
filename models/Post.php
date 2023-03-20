d5e87810c4427808e93d749905da8f8505a34afa
Arun P S <arun.ps@infospica.com>
2023-03-20 20:29:17 +0530

    update

--- models/Post.php
+++ models/Post.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 error_reporting(E_ALL);
-ini_set('display_error',1);
+ini_set('display_error', 1);
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/php/php-pdo/vendor/autoload.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/php/php-pdo/vendor/autoload.php');
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
@@ -19,7 +19,8 @@ use Firebase\JWT\Key;
  *          securityScheme="bearerToken",
  *      ),
  */
-class Post{
+class Post
+{
 
     //Post Properties
 
@@ -33,8 +34,9 @@ class Post{
 
     private $connection;
     private $table = 'posts';
-    
-    public function __construct($db){
+
+    public function __construct($db)
+    {
         $this->connection = $db;
     }
 
@@ -48,28 +50,27 @@ class Post{
      * )
      */
 
-public function auth()
-{
-  try{
-    $issueDate = time();
-    $expirationDate = time()*3600; //hour
-    $payload = [
-        'iss' => 'http://localhost/php/php-pdo',
-        'aud' => 'http://localhost',
-        'iat' => $issueDate,
-        'exp' => $expirationDate,
-        'userName' => 'Ajish'
-    ];
-    $jwtGeneratedToken = JWT::encode($payload, $this->key, 'HS256');
-    return [
-        'token' => $jwtGeneratedToken,
-        'expries' => $expirationDate,
-    ];
-  }
-  catch(PDOException $e){
-    echo $e->getMessage();
-  }
-}
+    public function auth()
+    {
+        try {
+            $issueDate = time();
+            $expirationDate = time() * 3600; //hour
+            $payload = [
+                'iss' => 'http://localhost/php/php-pdo',
+                'aud' => 'http://localhost',
+                'iat' => $issueDate,
+                'exp' => $expirationDate,
+                'userName' => 'Ajish'
+            ];
+            $jwtGeneratedToken = JWT::encode($payload, $this->key, 'HS256');
+            return [
+                'token' => $jwtGeneratedToken,
+                'expries' => $expirationDate,
+            ];
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 
     /**
      * @OA\Get(
@@ -83,36 +84,35 @@ public function auth()
      */
     public function readPosts()
     {
-        try{
+        try {
             $header = apache_request_headers();
-            if(isset($header["Authorization"])){
-                $token =str_ireplace('Bearer ', '', $header["Authorization"]);
+            if (isset($header["Authorization"])) {
+                $token = str_ireplace('Bearer ', '', $header["Authorization"]);
                 $decoded = JWT::decode($token, new Key($this->key, 'HS256'));
-                if(isset($decoded->userName) && $decoded->userName == "Ajish"){
+                if (isset($decoded->userName) && $decoded->userName == "Ajish") {
 
                     //query for reading posts from table.
-                    $query='SELECT category.name as category, posts.id,posts.title,
+                    $query = 'SELECT category.name as category, posts.id,posts.title,
                     posts.created_at,posts.description,posts.category_id
-                    FROM '.$this->table.' posts LEFT JOIN category ON posts.category_id = category.id
-                    ORDER BY posts.id ASC'; 
+                    FROM ' . $this->table . ' posts LEFT JOIN category ON posts.category_id = category.id
+                    ORDER BY posts.id ASC';
                     $post = $this->connection->prepare($query);
                     $post->execute();
                     return $post;
-                }else{
+                } else {
                     return false;
                 }
-           
-            }else{
+
+            } else {
                 return false;
             }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
         }
-        catch(PDOException $e){
-          echo $e->getMessage();
-        }
-        
+
     }
 
-    
+
     /**
      * @OA\Get(
      *   path="/php/php-pdo/api/post/single.php",
@@ -129,24 +129,41 @@ public function auth()
      *   ),
      *   @OA\Response(response=200,description="found"),
      *   @OA\Response(response=404,description="not found"),
+     *   security={ {"bearerToken":{}} }
      * )
      */
     public function readSinglePosts($id)
     {
-        $this->id = $id;
-       //query for reading posts from table.
-       $query='SELECT category.name as category, posts.id,posts.title,
-       posts.created_at,posts.description,posts.category_id
-       FROM '.$this->table.' posts LEFT JOIN category ON posts.category_id = category.id
-       WHERE posts.id=? LIMIT 0,1'; 
-       $post = $this->connection->prepare($query);
-    //    $post->bindValue('id', $this->id,PDO::PARAM_INT);
-    //    $post->execute();
-    //    $post->execute([$this->id]);
-        $post->bindValue(1, $this->id,PDO::PARAM_INT);
-        $post->execute();
+        try {
+            $header = apache_request_headers();
+            if (isset($header["Authorization"])) {
+                $token = str_ireplace('Bearer ', '', $header["Authorization"]);
+                $decoded = JWT::decode($token, new Key($this->key, 'HS256'));
+                if (isset($decoded->userName) && $decoded->userName == "Ajish") {
+                    $this->id = $id;
+                    //query for reading posts from table.
+                    $query = 'SELECT category.name as category, posts.id,posts.title,
+                            posts.created_at,posts.description,posts.category_id
+                            FROM ' . $this->table . ' posts LEFT JOIN category ON posts.category_id = category.id
+                            WHERE posts.id=? LIMIT 0,1';
+                    $post = $this->connection->prepare($query);
+                    //    $post->bindValue('id', $this->id,PDO::PARAM_INT);
+                    //    $post->execute();
+                    //    $post->execute([$this->id]);
+                    $post->bindValue(1, $this->id, PDO::PARAM_INT);
+                    $post->execute();
 
-       return $post;
+                    return $post;
+                } else {
+                    return false;
+                }
+
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 
     /**
@@ -178,37 +195,54 @@ public function auth()
      *   ),
      *   @OA\Response(response=200,description="found"),
      *   @OA\Response(response=404,description="not found"),
+     *   security={ {"bearerToken":{}} }
      * )
      */
     public function createPosts($params)
     {
-       try{
-            // Assigning Values
-            $this->title = $params['title'];
-            $this->category_id = $params['category_id'];
-            $this->description = $params['description'];
-            //Query to store new  post in database
-            $query = 'INSERT INTO '.$this->table.'
-            SET 
-                title = :title,
-                category_id = :category_id,
-                description = :description';
-                $post = $this->connection->prepare($query);
-                $post->bindValue('title',$this->title );
-                $post->bindValue('category_id',$this->category_id );
-                $post->bindValue('description',$this->description );
-                
-                if($post->execute()){
-                    return true;
+        try {
+            $header = apache_request_headers();
+            if (isset($header["Authorization"])) {
+                $token = str_ireplace('Bearer ', '', $header["Authorization"]);
+                $decoded = JWT::decode($token, new Key($this->key, 'HS256'));
+                if (isset($decoded->userName) && $decoded->userName == "Ajish") {
+
+                    try {
+                        // Assigning Values
+                        $this->title = $params['title'];
+                        $this->category_id = $params['category_id'];
+                        $this->description = $params['description'];
+                        //Query to store new  post in database
+                        $query = 'INSERT INTO ' . $this->table . '
+                                SET 
+                                    title = :title,
+                                    category_id = :category_id,
+                                    description = :description';
+                        $post = $this->connection->prepare($query);
+                        $post->bindValue('title', $this->title);
+                        $post->bindValue('category_id', $this->category_id);
+                        $post->bindValue('description', $this->description);
+
+                        if ($post->execute()) {
+                            return true;
+                        }
+                        return false;
+                    } catch (PDOException $e) {
+                        echo $e->getMessage();
+                    }
+                } else {
+                    return false;
                 }
+
+            } else {
                 return false;
-       }
-       catch(PDOException $e){
+            }
+        } catch (PDOException $e) {
             echo $e->getMessage();
-       }
+        }
     }
 
-    
+
     /**
      * @OA\Put(
      *   path="/php/php-pdo/api/post/update.php",
@@ -243,43 +277,59 @@ public function auth()
      *   ),
      *   @OA\Response(response=200,description="found"),
      *   @OA\Response(response=404,description="not found"),
+     *   security={ {"bearerToken":{}} }
      * )
      */
     public function updatePost($params)
     {
-        try{
-            // Assigning Values
-            $this->id = $params['id'];
-            $this->title = $params['title'];
-            $this->category_id = $params['category_id'];
-            $this->description = $params['description'];
+        try {
+            $header = apache_request_headers();
+            if (isset($header["Authorization"])) {
+                $token = str_ireplace('Bearer ', '', $header["Authorization"]);
+                $decoded = JWT::decode($token, new Key($this->key, 'HS256'));
+                if (isset($decoded->userName) && $decoded->userName == "Ajish") {
+                    try {
+                        // Assigning Values
+                        $this->id = $params['id'];
+                        $this->title = $params['title'];
+                        $this->category_id = $params['category_id'];
+                        $this->description = $params['description'];
 
-            //Query to update post in database
-            $query = 'UPDATE '.$this->table.' 
-            SET 
-                title = :title,
-                category_id = :category_id,
-                description = :description
-                WHERE id = :id';
-                $post = $this->connection->prepare($query);
+                        //Query to update post in database
+                        $query = 'UPDATE ' . $this->table . ' 
+                                SET 
+                                    title = :title,
+                                    category_id = :category_id,
+                                    description = :description
+                                    WHERE id = :id';
+                        $post = $this->connection->prepare($query);
 
-                $post->bindValue('id',$this->id );
-                $post->bindValue('title',$this->title );
-                $post->bindValue('category_id',$this->category_id );
-                $post->bindValue('description',$this->description );
-                
-                if($post->execute()){
-                    return true;
+                        $post->bindValue('id', $this->id);
+                        $post->bindValue('title', $this->title);
+                        $post->bindValue('category_id', $this->category_id);
+                        $post->bindValue('description', $this->description);
+
+                        if ($post->execute()) {
+                            return true;
+                        }
+                        return false;
+
+                    } catch (PDOException $e) {
+                        echo $e->getMessage();
+                    }
+                } else {
+                    return false;
                 }
+
+            } else {
                 return false;
-            
-       }
-       catch(PDOException $e){
+            }
+        } catch (PDOException $e) {
             echo $e->getMessage();
-       }
+        }
     }
 
-    
+
     /**
      * @OA\Delete(
      *   path="/php/php-pdo/api/post/delete.php",
@@ -299,29 +349,44 @@ public function auth()
      *   ),
      *   @OA\Response(response=200,description="found"),
      *   @OA\Response(response=404,description="not found"),
+     *   security={ {"bearerToken":{}} }
      * )
      */
     public function deletePost($id)
     {
-        try{
-            // Assigning Values
-            $this->id = $id;
-            //Query to update post in database
-            $query = 'DELETE FROM '.$this->table.' 
-                WHERE id = :id';
-                $post = $this->connection->prepare($query);
+        try {
+            $header = apache_request_headers();
+            if (isset($header["Authorization"])) {
+                $token = str_ireplace('Bearer ', '', $header["Authorization"]);
+                $decoded = JWT::decode($token, new Key($this->key, 'HS256'));
+                if (isset($decoded->userName) && $decoded->userName == "Ajish") {
+                    try {
+                        // Assigning Values
+                        $this->id = $id;
+                        //Query to update post in database
+                        $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+                        $post = $this->connection->prepare($query);
 
-                $post->bindValue('id',$this->id );
-                
-                if($post->execute()){
-                    return true;
+                        $post->bindValue('id', $this->id);
+
+                        if ($post->execute()) {
+                            return true;
+                        }
+                        return false;
+
+                    } catch (PDOException $e) {
+                        echo $e->getMessage();
+                    }
+                } else {
+                    return false;
                 }
+
+            } else {
                 return false;
-            
-       }
-       catch(PDOException $e){
+            }
+        } catch (PDOException $e) {
             echo $e->getMessage();
-       }
+        }
     }
 
 }
